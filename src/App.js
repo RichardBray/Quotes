1d79d0cde6d86f8064f4a0c89b847569422348af
Richard Bray (Local) <richardobray@yahoo.co.uk>
2018-09-20 18:39:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { data } from './quotes.json';
+
+
+class App extends Component {
+
+  state = {
+    quoteID: 0
+  }
+
+  /**
+   * Returns a random number based on the amount of data provuded
+   */
+  randomNum() {
+    const rand = Math.floor(Math.random() * data.length)
+    if (this.state.quoteID === rand) {
+      console.log('same number hit')
+      return this.randomNum()
+    }
+    return rand
+  }
+  
+  newQuote () {
+    this.setState({ quoteID: this.randomNum()})
+  }
+
+  render() {
+    const selected = data[this.state.quoteID]
+
+    return (
+      <View style={styles.quotesPageStyle}>
+        <Text>{ selected.quote }</Text>
+        <Text>{ selected.source}</Text>
+        <Button 
+          onPress={() => this.newQuote()}
+          title="Refresh"
+        />
+      </View>
+    )
+  }
+}
+
+const styles = {
+  quotesPageStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}
+
+export default App;
